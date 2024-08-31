8c5126083daf9d01e2af8ed7adc96648c0e4fe0d
eden <eden.quan.chen@gmail.com>
2024-09-01 01:23:32 +0800

    init

---
+++ generator/flatten.go
@@ -0,0 +1,36 @@
+package generator
+
+import (
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/proto"
+
+	"github.com/sinsay/protoc-gen-openapi-fx/generator/meta"
+)
+
+type FlattenInfo struct {
+	Rule    *meta.FlattenRules
+	Message *protogen.Message
+	Field   *protogen.Field
+}
+
+func ExtractFlatten(message *protogen.Message, field *protogen.Field) (*FlattenInfo, bool) {
+	flatten := proto.GetExtension(field.Desc.Options(), meta.E_Flatten).(bool)
+	ext := proto.GetExtension(field.Desc.Options(), meta.E_FlattenRule).(*meta.FlattenRules)
+
+	if ext == nil {
+		// create default ext
+		m := int32(0)
+		ext = &meta.FlattenRules{Reserved: &meta.Reserved{
+			Min: &m, // useless now
+			Max: &m, // useless now
+		}}
+	} else {
+		flatten = true
+	}
+
+	return &FlattenInfo{
+		Rule:    ext,
+		Message: message,
+		Field:   field,
+	}, flatten
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+//module github.com/google/gnostic
+module github.com/sinsay/protoc-gen-openapi-fx
+
+go 1.12
+
+require (
+	github.com/google/gnostic v0.7.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20230525234035-dd9d682886f9
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19
+	google.golang.org/protobuf v1.30.0
+)
